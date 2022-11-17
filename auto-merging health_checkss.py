b686a9741195261e27bf553f7e6cc65c593d23df
kienvtm <32055895+kienvtm@users.noreply.github.com>
2022-11-17 22:36:54 +0700

    Add check_cpu_constrained function.

--- auto-merging health_checkss.py
+++ auto-merging health_checkss.py
@@ -23,11 +23,15 @@ def check_disk_full(disk, min_gb, min_pct):
 def check_root_full():
     '''Returns True if the root partition is full, False otherwise.'''
     return check_disk_full(disk='/', min_gb=2, min_pct=10)
+def check_cpu_constrained():
+    '''Returns True if the cpu is having too much usage, False otherwise'''
+    return psutil.cpu_percent(1) > 75
 
 def main():
     checks = [
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
+        (check_cpu_constrained, "CPU overloaded")
     ]
     everything_ok = True
     for check, msg in checks:  
